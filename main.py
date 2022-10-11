1bc4d91b26606242604d61f3d750b8169b204542
sr_basic <zmsdkdl07@gmail.com>
2022-10-11 11:18:06 +0900

    입모양 구현을 위한 판정 추가 (mouth_judge부분 애니메이션 추후 구현하세요)

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import avatar
 import numpy as np
 
 '''
-ratio라고 찾아보시면 이 값을 기준으로 눈을 감았다 라고 판정하는 값이 있는데
+blink_judge라고 찾아보시면 이 값을 기준으로 눈을 감았다 라고 판정하는 값이 있는데
 이 값을 딱히 만지지 않아도 감지가 될만큼 코드를 최적화 했습니다.
 다만 카메라와의 거리에 따라 민감도가 달라지기도 하고,
 만약 처음 코드를 봤을때처럼 눈을 감은 판정이 이 수치에 따라 민감하게 반응하는 경우
@@ -24,6 +24,7 @@ TOTAL_BLINKS =0  # 눈을 몇번 깜빡였는 지 알 수 있는 변수, CEF_COU
 CLOSED_EYES_FRAME =3                # 눈을 깜빡였는지 판정하는 함수, n프레임만큼 감았다고 판정할시 시스템적으로 감았다 라고 판정
 FONTS =cv2.FONT_HERSHEY_COMPLEX     # 카메라실행시 옆에보이는 글자들 폰트
 
+blink_judge = 5.5        # 눈을 감은 판정을 결정하는 종횡비 수 (기본값 5.5)
 blink_animation = 0      # 눈을 감은 판정이 떴을때 1, 아닐때 0
 
 mp_drawing = mp.solutions.drawing_utils
@@ -92,6 +93,8 @@ def blinkRatio(img, landmarks, right_indices, left_indices):
 
     ratio = (reRatio+leRatio)/2         #평균값
     return ratio
+
+
 # 눈의 감았다 판정이 종횡비추정으로 구하는것이 반응이 좋아 입도 같은방식으로 진행
 # 종횡비로 구하는 이유는 카메라와 사람과의 거리(깊이)에 관계없이 판정을 낼 수 있는 수단이기 때문
 def mouthRatio(img, landmarks, mouth_indices,test = False) :
@@ -118,6 +121,27 @@ def mouthRatio(img, landmarks, mouth_indices,test = False) :
 
     return ratio
 
+# 입 종횡비에 따라 어떤 입모양을 출력할 지 결정하는 함수 , 아바타모드가 켜져있다면 avatar.py에서 구현(예정)
+# 레이어 합성을통한 캐릭터 생성 이후 애니메이션 구현예정
+def mouth_judge (mouth_ratio,avatar_mode) :
+    mouth = 0
+    if mouth_ratio <= 1 :
+        utils.colorBackgroundText(frame, f'm.pic3 bgmouth', FONTS, 1.0, (int(frame_height / 2), 50), 2, utils.YELLOW, pad_x=6, pad_y=6, )
+        mouth = 3
+    elif mouth_ratio <= 2.5 :
+        utils.colorBackgroundText(frame, f'm.pic2 mlmouth', FONTS, 1.0, (int(frame_height / 2), 50), 2, utils.YELLOW,
+                                  pad_x=6, pad_y=6, )
+        mouth = 2
+    elif mouth_ratio <= 10 :
+        utils.colorBackgroundText(frame, f'm.pic1 msmouth', FONTS, 1.0, (int(frame_height / 2), 50), 2, utils.YELLOW,
+                                  pad_x=6, pad_y=6, )
+        mouth = 1
+    else :
+        utils.colorBackgroundText(frame, f'm.pic0 nomouth', FONTS, 1.0, (int(frame_height / 2), 50), 2, utils.YELLOW,
+                                  pad_x=6, pad_y=6, )
+        mouth = 0
+    if avatar_mode :
+        return 0
 # test_mode일때만 작동, 출력중인 카메라에 눈을 선으로 그려줍니다.
 def test_draw_eyeline(img,mesh_coords,test_mode) :
     if test_mode :
@@ -161,18 +185,18 @@ if __name__ == "__main__" :         # main 함수
                 for face_landmarks in results.multi_face_landmarks:
                     mesh_coords = landmarksDetection(frame, results, False) # True면 감지된 모든 랜드마크를 바로바로 보여줍니다.
                     eye_ratio = blinkRatio(frame, mesh_coords, RIGHT_EYE, LEFT_EYE)
-                    utils.colorBackgroundText(frame, f'Ratio : {round(eye_ratio, 2)}', FONTS, 0.7, (30, 100), 2, utils.PINK,
+                    utils.colorBackgroundText(frame, f'Eye Ratio : {round(eye_ratio, 2)}', FONTS, 0.7, (30, 100), 2, utils.PINK,
                                               utils.YELLOW)
 
-                    # ratio값을 변경하여 눈인식의 개인차 조정 가능
+                    # blink_judge값을 변경하여 눈인식의 개인차 조정 가능
                     #
-                    if eye_ratio > 5.5:
+                    if eye_ratio > blink_judge:
                         CEF_COUNTER += 1
                         blink_animation = 1
                         # cv.putText(frame, 'Blink', (200, 50), FONTS, 1.3, utils.PINK, 2)
                         utils.colorBackgroundText(frame, f'Blink!', FONTS, 1.7, (int(frame_height / 2), 100), 2, utils.YELLOW, pad_x=6, pad_y=6, )
 
-                    # else함수에 있는 내용은 깜빡인 횟수를 카운팅하는 내용인데 현재 목표로하는 구현내용은 이 기능이 필요없긴함. 그래도 일단은 넣어봄
+                    # else함수에 있는 내용은 깜빡인 횟수를 카운팅하는 내용, 눈의 애니메이션을 넣는 활동서 사용
                     else :
                         if CEF_COUNTER > CLOSED_EYES_FRAME:
                             TOTAL_BLINKS += 1
@@ -189,6 +213,11 @@ if __name__ == "__main__" :         # main 함수
                     blink_animation = 0
 
                     mouth_ratio = mouthRatio(frame,mesh_coords, MOUTH, test=False)
+                    utils.colorBackgroundText(frame, f'Mouth Ratio : {round(mouth_ratio, 2)}', FONTS, 0.7, (30, 200), 2,
+                                              utils.PINK, utils.YELLOW)
+                    mouth_judge(mouth_ratio,avatar_mode)
+
+
                     # 아래는 극 초기 mediapipe에서 제공하는 기본 툴로 간단하게 카메라가 돌아가고 얼마나 감지했다 는 내용을 안 내용
                     # 얼굴감지를 수동으로 구현하였으므로 현재는 사용되지 않음, 하지만 일단 남겨봄
                     # 만약 가상 캐릭터와 매칭을 시킨다면 아래내용은 제외
