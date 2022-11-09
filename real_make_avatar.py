fc2528c4a825fd87700e380517f414726e0f2707
sr_basic <zmsdkdl07@gmail.com>
2022-11-09 14:32:09 +0900

    얼굴의 표정만 합성하는 make_face함수 구현

--- real_make_avatar.py
+++ real_make_avatar.py
@@ -12,6 +12,8 @@ main_body = rd.get_full_img_verPIL('./img/body/main_body.png', visual_mode)
 cloth = rd.get_full_img_verPIL('./img/body/cloth.png', visual_mode)
 hair = rd.get_full_img_verPIL('./img/head_parts/hair.png',visual_mode)
 
+class_labels=['Angry','Happy','Neutral','Sad','Surprise']
+
 
 # image_without_alpha = main_body[:,:,:3]                             # 전체
 # bg = np.full(image_without_alpha.shape,(0,215,0), dtype=np.uint8)   # 크로마키 배경이미지
@@ -60,6 +62,38 @@ def make_body(layer_count) :
         main_body = Image.alpha_composite(main_body, main_head)
         '''
 
+# for test
+eyes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+mouth = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+def make_face(eyes123=[],mouth123=[]) :
+    face=[]
+    emotion = []
+    mouths = []
+    i = 1
+    for eye in range(len(eyes)) :
+        for mou in range(len(mouth)) :
+            if eye//3 != mou//4 :
+                mou = (eye//3)*4+mou
+            # 여기에 얼굴과 입 합성 함수
+            mouths.append(i)
+            i += 1
+            print("face[",eye//3,"][",eye%3,"][",mou%4,"] = ",eyes[eye]," + ",mouth[mou])
+            if mou%4 == 3 :
+                print("/n")
+                face.append(mouths)
+                mouths=[]
+                break
+
+        if eye%3 == 2 :
+            emotion.append(face)
+            face=[]
+
+
+    for i in range(5) :
+        print(emotion[i])
+
+
+
 def check_exception(index):
     # Checks if any of the layers have a known exception with another layer
     temp_file_path = index
@@ -75,8 +109,6 @@ def is_exception(file_path):
 def main():
 
     # start_time = time.perf_counter()
-
-
 
 
     # main_body = Image.alpha_composite(main_body, main_head)
@@ -95,10 +127,9 @@ def main():
     frame2.show()
 
 
-
     # elapsed = (time.perf_counter() - start_time) * 1000
     # print ( "수행시간 = %0.2f ms" % elapsed)
-    cv2.waitKey(0) & 0xFF == 27
+    # cv2.waitKey(0) & 0xFF == 27
 
 if __name__ == "__main__":
     main()
